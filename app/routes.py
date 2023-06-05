ea81248482d34c24540183aecce5ad94fa75539f
Abdullah-atia <95718050+Abdullah-atia@users.noreply.github.com>
2023-06-05 10:47:10 +0300

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -104,10 +104,6 @@ def check_photo():
 def error_404(error):
     return render_template('errors/404.html'), 404
 
-@app.errorhandler(403)
-def error_403(error):
-    return render_template('errors/403.html'), 403
-
 @app.errorhandler(500)
 def error_500(error):
     return render_template('errors/500.html'), 500
